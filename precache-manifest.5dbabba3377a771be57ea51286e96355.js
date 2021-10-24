e4d3ee3a00fe1bde63da0581b2c04d611101aaaf
Prince Maurya <princemaurya0027@gmail.com>
2021-10-24 17:11:27 +0530

    Updates

---
+++ precache-manifest.5dbabba3377a771be57ea51286e96355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0b008a949e6cb8a1d2709869073948",
+    "url": "/sign1/ReactComputerVisionTemplate/index.html"
+  },
+  {
+    "revision": "006cbdb2368fc7efdd75",
+    "url": "/sign1/ReactComputerVisionTemplate/static/css/main.133405c3.chunk.css"
+  },
+  {
+    "revision": "874ef33877269dd92e48",
+    "url": "/sign1/ReactComputerVisionTemplate/static/js/2.6a56a3d3.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/sign1/ReactComputerVisionTemplate/static/js/2.6a56a3d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006cbdb2368fc7efdd75",
+    "url": "/sign1/ReactComputerVisionTemplate/static/js/main.91faf068.chunk.js"
+  },
+  {
+    "revision": "1bdb70e45a64e364ca14",
+    "url": "/sign1/ReactComputerVisionTemplate/static/js/runtime-main.fcea1717.js"
+  }
+]);
